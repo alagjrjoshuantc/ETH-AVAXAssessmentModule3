e12613cb4577217a157160ba11dfa82b2cac0ca3
Josh <123949250+alagjrjoshuantc@users.noreply.github.com>
2024-05-18 11:36:51 +0800

    Update JoshiToken.sol

--- JoshiToken.sol
+++ JoshiToken.sol
@@ -1,8 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// Create your own ERC20 token
-
+// Import the OpenZeppelin ERC20 contract to create your own ERC20 token
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract JoshiToken is ERC20 {
@@ -23,5 +22,22 @@ contract JoshiToken is ERC20 {
 
     function burn(uint256 amount) external {
         _burn(msg.sender, amount);
+    }
+
+    function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
+        require(recipient != address(0), "transfer: cannot transfer to the zero address");
+        require(amount > 0, "transfer: transfer amount must be greater than zero");
+        require(balanceOf(msg.sender) >= amount, "transfer: sender does not have enough tokens");
+        
+        return super.transfer(recipient, amount);
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public virtual override returns (bool) {
+        require(sender != address(0), "transferFrom: transfer from the zero address");
+        require(recipient != address(0), "transferFrom: transfer to the zero address");
+        require(amount > 0, "transferFrom: transfer amount must be greater than zero");
+        require(balanceOf(sender) >= amount, "transferFrom: sender does not have enough tokens");
+
+        return super.transferFrom(sender, recipient, amount);
     }
 }
